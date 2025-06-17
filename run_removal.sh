32978e0fb10ce4b97d1f3d7465038fbc52bdf477
Xiangpeng Yang <xianyang@neptune6.ihpc.uts.edu.au>
2025-06-18 01:52:33 +1000

    .

--- run_removal.sh
+++ run_removal.sh
@@ -1,3 +1,3 @@
-# CUDA_VISIBLE_DEVICES=0,1,2,3 python qwen_filter_edit_obj_removal.py
+CUDA_VISIBLE_DEVICES=0,1,2,3 python inference_obj_removal.py
 
-CUDA_VISIBLE_DEVICES=1,3 python inference_obj_removal.py
+# CUDA_VISIBLE_DEVICES=1,3 python inference_obj_removal.py
