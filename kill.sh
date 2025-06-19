c6d5d3804fcfd127e46a7fc9b127eda687e6952b
Xiangpeng Yang <knightyxp@gmail.com>
2025-06-20 00:19:49 +1000

    new

--- kill.sh
+++ kill.sh
@@ -1,2 +1,2 @@
-ps -ef | grep qwen_filter_edit_obj_addition.py | grep -v grep | awk '{print $2}' | xargs kill -9
-ps -ef | grep run_filter.sh | grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef | grep preprocess_senorita.py | grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef | grep inference_vie_score_filter.py | grep -v grep | awk '{print $2}' | xargs kill -9
